06cadc6540f0fd3b34ea59bd170f18562b521105
Killacchin <93428533+Killacchin@users.noreply.github.com>
2021-10-31 22:56:44 -0300

    Update RAFTP.lua

--- RAFTP.lua
+++ RAFTP.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("RFA Project - 0.05", "Synapse")
+local Window = Library.CreateLib("RFA Project - 0.06", "Synapse")
 
 local AutoFarm = Window:NewTab("AutoFarm")
 
